3fc93669e305af6c3638c83f69e3a22475bd9c2a
Mitchel Cabuloy <mixxorz@gmail.com>
2016-10-25 15:41:43 +0800

    Add Django 1.10 to test matrix
    * Add TEMPLATES setting to runtests to make it work with 1.10

--- runtests.py
+++ runtests.py
@@ -21,7 +21,25 @@ settings.configure(
                     'svg',),
     STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static')],
     STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles'),
-    STATIC_URL='/static/'
+    STATIC_URL='/static/',
+    TEMPLATES=[
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [],
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                    'django.template.context_processors.i18n',
+                    'wagtail.contrib.settings.context_processors.settings',
+                    'apps.shop.context_processors.collections',
+                ],
+            },
+        },
+    ]
 )
 django.setup()
 
